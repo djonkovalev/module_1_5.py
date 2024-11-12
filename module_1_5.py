721776c38c0f21e4d1bf1529b24c77b4d1ae38eb
djonkovalev <djon9489@gmail.com>
2024-11-12 22:16:35 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, 3, 4, "strong , bool , fools "
+ # кортеж является неизменяемая колекция
+mutable_list = (1, 2, 3, 4, ["windows", 7, 8, 10, 11],)
+mutable_list [4] [4]= 22
+print(mutable_list)
+
+
